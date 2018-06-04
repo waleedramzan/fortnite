d837a15886b90e3dbf41f3c9ef42869e8c903588
Waleed Ramzan <mr.ramzan07@gmail.com>
2018-06-05 01:33:27 +0500

    yo

--- static/plugins/lightbox/lightbox.js
+++ static/plugins/lightbox/lightbox.js
@@ -27,7 +27,7 @@
 			  '<div class="title_cosmetics">'+ option.description +'</div>' +
 	      '<div id="disqus_thread"></div>' +
 	      '<script>' +
-	        "(function() { var d = document, s = d.createElement('script'); s.src = 'https://" + light.disqus + ".disqus.com/embed.js';s.setAttribute('data-timestamp', +new Date()); (d.head || d.body).appendChild(s); })();" +
+			  "(function() {var d = document, s = d.createElement('script');s.src = 'https://fortnitego-com.disqus.com/embed.js';s.setAttribute('data-timestamp', +new Date());(d.head || d.body).appendChild(s);})();" +
 	      '</script>' +
 	      '<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>' +
 	      '<script id="dsq-count-scr" src="https://' + light.disqus  + '.disqus.com/count.js" async></script>' +
